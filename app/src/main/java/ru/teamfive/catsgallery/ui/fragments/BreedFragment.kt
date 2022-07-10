0999f3c68a3f6a3f6bfba76433b5521858a938d4
Elmira7 <gilfanova-03@inbox.ru>
2022-07-10 23:44:45 +0300

    породы,пока не берутся данные(ошибка исправлена)

--- app/src/main/java/ru/teamfive/catsgallery/ui/fragments/BreedFragment.kt
+++ app/src/main/java/ru/teamfive/catsgallery/ui/fragments/BreedFragment.kt
@@ -7,20 +7,19 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import com.teamfive.catsgallery.R
 import com.teamfive.catsgallery.databinding.ActivityMainBinding
+import com.teamfive.catsgallery.databinding.FragmentBreedBinding
 import ru.teamfive.catsgallery.ui.fragments.recycler.breed2.TemaAdapter
 import ru.teamfive.catsgallery.ui.fragments.recycler.breed2.TemaRep
 
-
-
-class BreedFragment:Fragment(R .layout.activity_main) {
-    private var _binding: ActivityMainBinding? = null
+class BreedFragment:Fragment(R .layout.fragment_breed) {
+    private var _binding: FragmentBreedBinding? = null
     private val binding get() = _binding!!
 
     private var adapter: TemaAdapter? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        _binding = ActivityMainBinding.bind(view)
+        _binding =  FragmentBreedBinding.bind(view)
 
         val text = arguments?.getString(ARG_TEXT).orEmpty()
 
