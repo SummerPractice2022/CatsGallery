724a6dfa5251c4b6e93a5b792b8ca0f6ab394203
nVoxel <caverockman@gmail.com>
2022-07-10 23:46:23 +0300

    Метод получения картинки по id

--- app/src/main/java/ru/teamfive/catsgallery/data/api/repositories/ImagesRepository.kt
+++ app/src/main/java/ru/teamfive/catsgallery/data/api/repositories/ImagesRepository.kt
@@ -1,6 +1,7 @@
 package ru.teamfive.catsgallery.data.api.repositories
 
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 import ru.teamfive.catsgallery.data.api.models.Image
 
@@ -23,4 +24,12 @@ interface ImagesRepository {
         @Query("size") size: String = "full",
         @Query("order") order: String = "RANDOM"
     ): List<Image>
+
+    /**
+     * Метод получает картинку по её id
+     * @param imageId id картинки
+     * @return Возвращает картинку с указанным id
+     */
+    @GET("/v1/images/{image_id}")
+    suspend fun getImageById(@Path("image_id") imageId: String): Image
 }
